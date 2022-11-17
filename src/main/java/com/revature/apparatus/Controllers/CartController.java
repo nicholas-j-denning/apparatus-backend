d55c3427367bd657273a3592f41127b05a38e34c
Vlad <vova96.1996@gmail.com>
2022-11-17 09:39:23 -0800

    Small unknown changes

--- src/main/java/com/revature/apparatus/Controllers/CartController.java
+++ src/main/java/com/revature/apparatus/Controllers/CartController.java
@@ -1,6 +1,5 @@
 package com.revature.apparatus.Controllers;
 
-import java.util.Optional;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.revature.apparatus.Interface.AuthenticatedUser;
 import com.revature.apparatus.Models.Cart;
